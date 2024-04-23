a5435c0b681d791c3bf58aeffa667925e0655a30
Bimal A. Kumar <bimal.kumar@fnu.ac.fj>
2024-04-23 13:28:25 +1200

    Simple React App

---
+++ src/Body.js
@@ -0,0 +1,18 @@
+
+function Body(props){
+
+return (
+	    <>
+		
+		<p> Web Site Body 	Hello {props.Name}</p>
+	   
+	    </>
+	)
+	    
+	
+;
+}
+
+export default Body;
+
+
---
+++ src/Footer.js
@@ -0,0 +1,8 @@
+
+
+function Footer(){
+
+return <h2> Website Footer </h2>
+}
+
+export default Footer;
